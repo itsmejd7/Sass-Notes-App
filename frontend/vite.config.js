bdb4d19cb0e451bac1733811ca0d0f21d8f753d7
Your Name <youremail@example.com>
2025-09-24 00:20:38 +0530

    Vercel Error Logs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [react()],
-  server: {
-    port: 5173,
-  },
+  base: '/frontend/',  
   build: {
     outDir: 'dist',
+  },
+  server: {
+    port: 5173,
   }
 })
